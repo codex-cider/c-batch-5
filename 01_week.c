4112b9bb38b4154b76c5d371302549409f45d39d
Prachi <prachimeena968@gmail.com>
2021-11-17 19:07:00 +0530

    week program work

---
+++ 01_week.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+void main()
+{
+    int week;
+    printf("enter the week number:");
+    scanf("%d", &week);
+
+    if (week == 1)
+    {
+        printf("MONDAY");
+    }
+    else if (week == 2)
+    {
+        printf("TUESDAY");
+    }
+    else if (week == 3)
+    {
+        printf("WEDNESDAY");
+    }
+    else if (week == 4)
+    {
+        printf("THRUSDAY");
+    }
+    else if (week == 5)
+    {
+        printf("FRIDAY");
+    }
+    else if (week == 6)
+    {
+        printf("SATURDAY");
+    }
+    else if (week == 7)
+    {
+        printf("SUNDAY");
+    }
+    else
+    {
+        printf("invalid input");
+    }
+}
