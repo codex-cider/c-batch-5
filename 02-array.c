298093fb89887b5e1b79004491e9aa1b5c8609ec
Rudransh waday <rudranshwaday2002@gmail.com>
2021-11-18 18:04:48 +0530

    sonal

---
+++ 02-array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a[5];
+     a[0]=50;
+     a[1]=70;
+     a[2]=80;
+     a[3]=90;
+     a[4]=30;
+     
+    printf("a[0]=%d\n");
+    printf("a[1]=%d\n");
+    printf("a[2]=%d\n");
+   printf("a[3]=%d\n");
+  printf("a[4]=%d\n");
+
+}
