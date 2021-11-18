75cab7b5451e69b9a207f0c48cca4aa1bb5ac56a
Prachi <prachimeena968@gmail.com>
2021-11-18 18:00:35 +0530

    array

---
+++ 02array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    //array is a collection of similar type of data
+    int a[5]; //declaration of array type variable
+    //10=size of array , inside the bracket we right size of array
+    a[0] = 50;
+    a[1] = 70;
+    a[2] = 90;
+    a[3] = 110;
+    a[4] = 130;
+
+    printf("a[0]=50\n");
+    printf("a[1]=70\n");
+    printf("a[2]=90\n");
+    printf("a[3]=110\n");
+    printf("a[4]=130\n");
+}
