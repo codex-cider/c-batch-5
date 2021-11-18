58bf26c8cd55667843585e39c7b820956338b853
Rudransh waday <rudranshwaday2002@gmail.com>
2021-11-18 18:24:58 +0530

    get the value from user

--- 03-user_array.c
+++ 03-user_array.c
@@ -2,15 +2,15 @@
 void main()
 {
     int a[5];
-    printf("enter the value :");
+    printf("enter the value of a[0] :");
     scanf("%d",&a[0]);
-    printf("enter the value :");
+    printf("enter the value of a[1]:");
     scanf("%d",&a[1]);
-    printf("enter the value :");
+    printf("enter the value of a[2] :");
     scanf("%d",&a[2]);
-    printf("enter the value :");
+    printf("enter the value of a[3]:");
     scanf("%d",&a[3]);
-    printf("enter the value :");
+    printf("enter the value of a[4] :");
     scanf("%d",&a[4]);
     printf("a[0]=%d\n",a[0]);
     printf("a[1]=%d\n",a[1]);
