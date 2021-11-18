ddcd34c2a2494e97c33f231ae345d8630dc9907d
chandrika bijore <chandrikabijore@gmail.com>
2021-11-18 18:32:21 +0530

    get the inpute in array from user

---
+++ 40-array_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+    int a[5];
+    printf("The value of a[0] is:\n");
+    scanf("%d", &a[0]);
+    printf("The value of a[1] is:\n");
+    scanf("%d", &a[1]);
+    printf("The value of a[2] is:\n");
+    scanf("%d", &a[2]);
+    printf("The value of a[3] is:\n");
+    scanf("%d", &a[3]);
+    printf("The value of a[4] is:\n");
+    scanf("%d", &a[4]);
+
+    printf("a[0]=%d\n", a[0]);
+    printf("a[1]=%d\n", a[1]);
+    printf("a[2]=%d\n", a[2]);
+    printf("a[3]=%d\n", a[3]);
+    printf("a[4]=%d\n", a[4]);
+}
