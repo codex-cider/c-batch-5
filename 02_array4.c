615ec89c70cac42e662c066ba8c007f48a0a05bb
Prachi <prachimeena968@gmail.com>
2021-11-19 17:33:16 +0530

    arrayindex

---
+++ 02_array4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+    int p;
+    printf("enter the value of p-");
+    scanf("%d", &p);
+    int a[p];
+    int i;
+
+    for (i = 0; i < p; i++)
+    {
+        printf("you entered the value of a[%d]:", i);
+        scanf("%d", &a[i]);
+    }
+
+    // printf("a[0]=%d\n", a[0]);
+    // printf("a[1]=%d\n", a[1]);
+    // printf("a[2]=%d\n", a[2]);
+    // printf("a[3]=%d\n", a[3]);
+    // printf("a[4]=%d\n", a[4]);
+
+    for (i = 0; i < p; i++)
+    {
+        printf("a[%d]=%d\n", i, a[i]);
+    }
+}
