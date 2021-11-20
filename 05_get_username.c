6dd40a09c12efcaaf6ef1e1269d6fca661675a2d
Prachi <prachimeena968@gmail.com>
2021-11-20 17:49:30 +0530

    username

---
+++ 05_get_username.c
@@ -0,0 +1,12 @@
+//to print the text
+
+#include <stdio.h>
+void main()
+{
+    char name[10];
+    printf("enter your name:");
+    scanf("%s", &name);
+
+    printf("\n\n");
+    printf("your name is : %s", name);
+}
