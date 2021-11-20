a5f9c2314e32bbf34f1debdc0d6ffd6d9fc5347f
sonal khetkar <khetkarsonal@gmail.com>
2021-11-20 17:54:32 +0530

    get_username

---
+++ 10-starprint_3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,k;
+    for (i=0;i<=5;i++)
+    {
+        printf(" \n");
+        
+        for (j=0;j<i;j++)
+        {
+            printf("* ");
+            
+        }
+        for (k=0;)
+    }
+}
