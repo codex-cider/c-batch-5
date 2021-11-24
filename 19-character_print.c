6c4375cfc909ce0cf3d67ffecbb7a7d379700656
sonal khetkar <khetkarsonal@gmail.com>
2021-11-24 18:38:06 +0530

    character print

---
+++ 19-character_print.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    char ch[5]={'a','e','i','o','u'};
+    for ( i=0;i<5;i++)
+    printf("ch[%d]=%c\n",i,ch[i]);
+    printf("\n");
+    printf("&ch[%d]=%d\n",i,&ch[i]);
+    printf("\n");
+
+}
