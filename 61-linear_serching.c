3c5d81b1e9411e0a28e702f237706620d226e7d4
chandrikabijore <chandrikabijore@gmail.com>
2021-11-25 18:47:29 +0530

    linear_search

---
+++ 61-linear_serching.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    int a[10];
+
+    for(i = 0; i < 10; i++)
+    {
+        printf("Enter the value of a[%d] = ", i);
+        scanf("%d",&a[i]);
+    }
+}
