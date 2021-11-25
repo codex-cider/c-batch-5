f536df7a1b33ce11d188355493f01a57fd440e35
Prachi <prachimeena968@gmail.com>
2021-11-25 18:19:20 +0530

    searching_linear

---
+++ 14_linear_serching.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void main()
+{
+    int a[10];
+
+    printf("a[0]:");
+    scanf("%d", &a[0]);
+    printf("a[1]:");
+    scanf("%d", &a[1]);
+    printf("a[2]:");
+    scanf("%d", &a[2]);
+    printf("a[3]:");
+    scanf("%d", &a[3]);
+    printf("a[4]:");
+    scanf("%d", &a[4]);
+    printf("a[5]:");
+    scanf("%d", &a[5]);
+    printf("a[6]:");
+    scanf("%d", &a[6]);
+    printf("a[7]:");
+    scanf("%d", &a[7]);
+    printf("a[8]:");
+    scanf("%d", &a[8]);
+    printf("a[9]:");
+    scanf("%d", &a[9]);
+    printf("a[10]:");
+    scanf("%d", &a[10]);
+}
