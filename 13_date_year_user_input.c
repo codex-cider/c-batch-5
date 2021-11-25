124ab4f53bd27ab6360acbc76942110a8aa35ec4
Prachi <prachimeena968@gmail.com>
2021-11-25 18:11:58 +0530

    date_month

--- 13_date_year_user_input.c
+++ 13_date_year_user_input.c
@@ -9,51 +9,52 @@ void main()
     scanf("%d", &date);
     printf("enter the month-\n");
     scanf("%d", &month);
-
-    switch (month)
-    {
-    case 1:
-        printf("JAN\n");
-        break;
-
-    case 2:
-        printf("FEB\n");
-        break;
-    case 3:
-        printf("MAR\n");
-        break;
-    case 4:
-        printf("APR\n");
-        break;
-    case 5:
-        printf("MAY\n");
-        break;
-    case 6:
-        printf("JUN\n");
-        break;
-    case 7:
-        printf("JUL\n");
-        break;
-    case 8:
-        printf("AUG\n");
-        break;
-    case 9:
-        printf("SEP\n");
-        break;
-    case 10:
-        printf("OCT\n");
-        break;
-    case 11:
-        printf("NOV\n");
-        break;
-    case 12:
-        printf("DEC\n");
-        break;
-    }
-
     int year;
     printf("enter the year-\n");
     scanf("%d", &year);
 
-    printf("%d%d%d", date, month, year);
+    printf("%d  ", date);
+
+    switch (month)
+    {
+    case 1:
+        printf("JAN  ");
+        break;
+
+    case 2:
+        printf("FEB  ");
+        break;
+    case 3:
+        printf("MAR  ");
+        break;
+    case 4:
+        printf("APR  ");
+        break;
+    case 5:
+        printf("MAY  ");
+        break;
+    case 6:
+        printf("JUN  ");
+        break;
+    case 7:
+        printf("JUL");
+        break;
+    case 8:
+        printf("AUG  ");
+        break;
+    case 9:
+        printf("SEP  ");
+        break;
+    case 10:
+        printf("OCT  ");
+        break;
+    case 11:
+        printf("NOV  ");
+        break;
+    case 12:
+        printf("DEC  ");
+        break;
+    }
+
+    printf("%d  ", year);
 }
