b927eb8f196f893ef8276e1a8e271daaccb99031
Prachi <prachimeena968@gmail.com>
2021-11-25 18:24:22 +0530

    searching_linear

--- 14_linear_serching.c
+++ 14_linear_serching.c
@@ -1,28 +1,13 @@
 #include <stdio.h>
 void main()
 {
-    int a[10];
 
-    printf("a[0]:");
-    scanf("%d", &a[0]);
-    printf("a[1]:");
-    scanf("%d", &a[1]);
-    printf("a[2]:");
-    scanf("%d", &a[2]);
-    printf("a[3]:");
-    scanf("%d", &a[3]);
-    printf("a[4]:");
-    scanf("%d", &a[4]);
-    printf("a[5]:");
-    scanf("%d", &a[5]);
-    printf("a[6]:");
-    scanf("%d", &a[6]);
-    printf("a[7]:");
-    scanf("%d", &a[7]);
-    printf("a[8]:");
-    scanf("%d", &a[8]);
-    printf("a[9]:");
-    scanf("%d", &a[9]);
-    printf("a[10]:");
-    scanf("%d", &a[10]);
+    int a[10];
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("you entered the value of a[%d]:", i);
+        scanf("%d", &a[i]);
+    }
 }
