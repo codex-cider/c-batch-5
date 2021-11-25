4bbe204d4232d90d1be9fab9a03da6614b3d016c
Prachi <prachimeena968@gmail.com>
2021-11-25 17:45:37 +0530

    date_month

--- 13_date_year_user_input.c
+++ 13_date_year_user_input.c
@@ -10,5 +10,44 @@ void main()
     printf("enter the month-\n");
     scanf("%d", &month);
 
-    printf("date is-%d-%s", date, month);
+    switch (month)
+    {
+    case 1:
+        printf("JAN");
+        break;
+
+    case 2:
+        printf("FEB");
+        break;
+    case 3:
+        printf("MAR");
+        break;
+    case 4:
+        printf("APR");
+        break;
+    case 5:
+        printf("MAY");
+        break;
+    case 6:
+        printf("JUN");
+        break;
+    case 7:
+        printf("JUL");
+        break;
+    case 8:
+        printf("AUG");
+        break;
+    case 9:
+        printf("SEP");
+        break;
+    case 10:
+        printf("OCT");
+        break;
+    case 11:
+        printf("NOV");
+        break;
+    case 12:
+        printf("DEC");
+        break;
+    }
 }
