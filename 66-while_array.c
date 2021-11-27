1c8d1889861e966942a70ea2976678c74290bd8c
chandrikabijore <chandrikabijore@gmail.com>
2021-11-27 18:26:06 +0530

    while_array

---
+++ 66-while_array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a[5];
+    int i=0;
+    
+    while ( i < 5)
+    {
+        printf("you Enter the value of array : a[%d] = ", i);
+        scanf("%d", &a[5]);
+        i++;
+    }
+}
