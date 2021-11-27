198e3e7fbb1d1cdbb39672d13c83895bf6b43991
Prachi <prachimeena968@gmail.com>
2021-11-27 18:29:17 +0530

    array_while_loop.c

--- 16_while_loop.c
+++ 16_while_loop.c
@@ -9,4 +9,10 @@ void main()
         scanf("%d", &a[i]);
         i++;
     }
+
+    printf("a[0]=%d\n", a[0]);
+    printf("a[1]=%d\n", a[1]);
+    printf("a[2]=%d\n", a[2]);
+    printf("a[3]=%d\n", a[3]);
+    printf("a[4]=%d\n", a[4]);
 }
