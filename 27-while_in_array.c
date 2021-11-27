e44c882ddf8441e41abdc564f566c2f73562fdc7
sonal khetkar <khetkarsonal@gmail.com>
2021-11-27 18:46:33 +0530

    array in while

--- 27-while_in_array.c
+++ 27-while_in_array.c
@@ -2,11 +2,17 @@
 void main()
 {
     int a[5];
-    int i;
+    int i=0;
     while(i<5)
     {
     printf("you entered the value of a[%d] :", i );
     scanf("%d",&a[i]);
     i++;
     }
+      while(i<5)
+      {
+        printf("a[%d]=%d\n", i, a[i]);
+        i++;
+      }
+    
 }
