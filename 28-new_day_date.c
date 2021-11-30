32b59aa442567dd5a6428911bdade3be0dd477d3
Rudransh waday <rudranshwaday2002@gmail.com>
2021-11-30 18:15:48 +0530

    next date

---
+++ 28-new_day_date.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int date, month, year, ndate;
+    printf("enter the date :");
+    scanf("%d", &date);
+    printf("enter the month :");
+    scanf("%d", &month);
+    printf("enter the year :");
+    scanf("%d", &year);
+    printf("%d/%d/%d",date,month,year);
+}
