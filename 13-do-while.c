07a59c9b0c00b6d35d7dd33cad18e03ed7da5ea2
Krishnakant Madhpuriya <kkmadhpuriya@gmail.com>
2021-12-06 19:06:23 +0530

    Function & Do while

---
+++ 13-do-while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+    /**
+     * @brief 
+     * 
+     * do{
+     * 
+     * }while( condition );
+     * 
+     */
+
+    int a = 20;
+
+    do
+    {
+        printf("Condition called");
+
+    } while (a > 30);
+}
---
+++ 14-functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void sum(); // function declaration
+
+// Function definations
+void sum()
+{
+    printf("Called SUM\n");
+}
+
+void main()
+{
+    sum(); // Function Calling
+}
