c05063c7272ee975c316906403f46e9632408649
sonal khetkar <khetkarsonal@gmail.com>
2021-12-06 18:40:07 +0530

    calculator

--- 28-next_day_date_year.c
+++ 28-next_day_date_year.c
@@ -1,58 +1,72 @@
- #include <stdio.h>
+#include <stdio.h>
 void main()
 {
-    int date,month,year,ndate,nmonth,nyear;
-    printf("enter the date :");
-    scanf ("%d",&date);
-    printf("enter the month :");
-    scanf ("%d",&month);
-    printf("enter the year :");
-    scanf ("%d",&year);
-    ndate=date+1;
-    nmonth=month+1;
-    nyear=year+1;
-    if (date<28)
+    int date, month, year;
+
+    printf("Enter any date : ");
+    scanf("%d", &date);
+    printf("Enter any month : ");
+    scanf("%d", &month);
+    printf("Enter any year : ");
+    scanf("%d", &year);
+
+    if (date < 28)
     {
         date++;
-        month=nmonth;
     }
     else
     {
-        if(date==29 && month==2 && year%4==0)
+        if (date == 28 && month == 2 && (year % 4) != 0)
         {
-            ndate=1;
-            
+            date = 1;
+            month++;
         }
-        else 
+        else if (date == 29 && month == 2 && (year % 4) == 0)
         {
-            ndate=1;
-            
+            date = 1;
+            month++;
         }
-        switch(month)
+        else if (date == 30)
         {
-            case 4:case 6:case 9:case 11:;
-            date==30;
-            ndate=1;
-            
+            switch (month)
+            {
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                date = 1;
+                month++;
+                break;
+            }
         }
-        switch(month)
+        else if (date == 31)
         {
-            case 1:case 3:case 5:case 7:case 8:case 10:case 12:;
-            date==31;
-            date=1;
-            
+            switch (month)
+            {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                date = 1;
+                month++;
+                break;
+            }
         }
-        
+        else
+        {
+            date++;
+        }
     }
-    if(month==13)
+    if (month == 13)
     {
-        month=1;
+        month = 1;
         year++;
     }
-    printf("%d/%d/%d\n",ndate,nmonth,nyear);
+    printf("%d/%d/%d\n", date, month, year);
 }
-
-
 
 
 
---
+++ 30-functions.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void sum();//function calling
+//function defination
+void sum()
+{                                                                               
+    printf("called sum\n");
+}
+void main()
+{
+    sum();//function calling
+}
