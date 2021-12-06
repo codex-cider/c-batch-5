ba37a33d29ff8c9385c44190e6c4bb539e81171f
chandrikabijore <chandrikabijore@gmail.com>
2021-12-06 18:40:27 +0530

    calculator

--- 67-Next_Date.c
+++ 67-Next_Date.c
@@ -1,4 +1,73 @@
-#include<stdio.h>
+#include <stdio.h>
+void main()
+{
+    int date, month, year;
+
+    printf("Enter any date : ");
+    scanf("%d", &date);
+    printf("Enter any month : ");
+    scanf("%d", &month);
+    printf("Enter any year : ");
+    scanf("%d", &year);
+
+    if (date < 28)
+    {
+        date++;
+    }
+    else
+    {
+        if (date == 28 && month == 2 && (year % 4) != 0)
+        {
+            date = 1;
+            month++;
+        }
+        else if (date == 29 && month == 2 && (year % 4) == 0)
+        {
+            date = 1;
+            month++;
+        }
+        else if (date == 30)
+        {
+            switch (month)
+            {
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                date = 1;
+                month++;
+                break;
+            }
+        }
+        else if (date == 31)
+        {
+            switch (month)
+            {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                date = 1;
+                month++;
+                break;
+            }
+        }
+        else
+        {
+            date++;
+        }
+    }
+    if (month == 13)
+    {
+        month = 1;
+        year++;
+    }
+    printf("%d/%d/%d", date, month, year);
+}
+/*#include<stdio.h>
 void main()
 {
     int date, month, year, nextdate,  nextmonth, nextyear;
@@ -47,10 +116,10 @@ void main()
             }
         }    
     }
- if(year==13)
- {
-   month=1;
-   year++;
- }
- printf("%d/%d/%d",date,month,year);
-}
+    if(month==13)
+    {
+        month=1;
+        year++;
+    }
+    printf("%d/%d/%d",date,month,year);
+}*/
---
+++ 70-calculator.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void sum();
+void sub();
+void mul();
+void div();
