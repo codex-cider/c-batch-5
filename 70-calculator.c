70d341fa73b1c68ad3551831219216fd8e131257
chandrikabijore <chandrikabijore@gmail.com>
2021-12-06 18:48:46 +0530

    calculator

--- 70-calculator.c
+++ 70-calculator.c
@@ -3,3 +3,22 @@ void sum();
 void sub();
 void mul();
 void div();
+
+void sum()
+{
+    printf("called sum");
+}
+
+void sub()
+{
+    printf("called sub");
+}
+void mul()
+{
+    printf("called mul");
+}
+
+void div()
+{
+    printf("called div");
+}
