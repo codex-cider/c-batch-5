f999f9a64d9e88f68d17cbd3684f3819408c9f56
chandrikabijore <chandrikabijore@gmail.com>
2021-12-06 17:50:44 +0530

    dmy

--- 67-Next_Date.c
+++ 67-Next_Date.c
@@ -13,4 +13,44 @@ void main()
     scanf("%d", &year);
     nextyear = year + 1;
     printf("Next date is : %d/%d/%d",nextdate, nextmonth, nextyear);
+
+    if ( date<28 )
+    {
+        date++;
+    }
+    else{
+        if (date == 28 && month == 2 && (year%4) != 0)
+        {
+            date=1;
+            month++;
+        }
+        else if (date == 29 && month == 2 && (year%4) == 0)
+        {
+            date=1;
+            month++;
+        }
+        else if (date == 30)
+        {
+            switch (month)
+            {
+                case 4 :case 6: case 9: case 11:
+                break;
+            }
+              month++;
+        }      
+        else if (date == 31)
+        {
+            switch (month)
+            {
+                case 1 :case 3: case 5: case 7: case 8: case 10: case 12:
+                break;
+            }
+        }    
+    }
+ if(year==13)
+ {
+   month=1;
+   year++;
+ }
+ printf("%d/%d/%d",date,month,year);
 }
