e8cf31ce11ad7898517900dcd5efaedf1a8a77c3
sonal khetkar <khetkarsonal@gmail.com>
2021-12-06 18:56:23 +0530

    calculator

--- 31-calculator.c
+++ 31-calculator.c
@@ -1,22 +1,32 @@
 #include<stdio.h>
 void sum();
+void sub();
+void mul();
+void div();
 void sum()
 {
     printf("called SUM");
 }
-void sub();
 void sub()
 {
     printf("called SUB");
 }
-void div();
-void div()
-{
-    printf("called DIV");
-}
-void mul();
 void mul()
 {
     printf("called MUL");
 }
+void div()
+{
+    printf("called DIV");
+}
+void main()
+{
+    int options;
+    printf("press 1 for sum\n");
+    printf("press 2 for sub\n");
+    printf("press 3 for mul\n");
+    printf("press 4 for div\n");
+    scanf("%d",&options);
+}
+
 
