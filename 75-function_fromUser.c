ad32a7109eba07de7e85833e5d27c300a96a6ec1
chandrikabijore <chandrikabijore@gmail.com>
2021-12-08 18:44:02 +0530

    function

--- 75-function_fromUser.c
+++ 75-function_fromUser.c
@@ -2,12 +2,17 @@
 void sum();
 void main()
 {
-    int a = 10;
-    int b = 20;
-    sum(a, b);
+    int a,b;
+    printf("Enter first number here : ");
+    scanf("%d",&a);
+    printf("Enter second number here : ");
+    scanf("%d",&b);
+    int c = a + b;
+    printf("Sum of this number is : %d\n",c);
+    sum(a, b);       // argument
 }
 
-void sum( int x, int y)
+void sum( int x, int y)   // perameter
 {
     printf("%d, %d",x, y);
 }
