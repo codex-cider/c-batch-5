3be503326ef03e66fce3a0a16b189dedf83056eb
chandrikabijore <chandrikabijore@gmail.com>
2021-12-09 13:56:46 +0530

    function

--- 75-function_fromUser.c
+++ 75-function_fromUser.c
@@ -1,16 +1,63 @@
 #include<stdio.h>
 void sum();
+void sub();
+void mul();
+void div();
+
 void main()
 {
     int a,b;
-    printf("Enter first number here : ");
+    printf("Enter number here for a : ");
     scanf("%d",&a);
-    printf("Enter second number here : ");
+    printf("Enter number here for b : ");
     scanf("%d",&b);
+    
+    int option;
+    printf("Press 1 for sum\n");
+    printf("Press 2 for sub\n");
+    printf("Press 3 for mul\n");
+    printf("Press 4 for div\n");
+    scanf("%d", &option);
+    if (option == 1)
+    {
+        sum(a,b);
+    }
+    else if (option == 2)
+    {
+        sub(a,b);
+    }
+    else if (option == 3)
+    {
+        mul(a,b);
+    }
+    else if (option == 4)
+    {
+        div(a,b);
+    }
+    else
+    {
+        printf("You enter wrong no.");
+    }
+
     sum(a, b);       // argument
+    sub(a, b);
+    mul(a, b);
+    div(a, b);
 }
 
 void sum( int x, int y)   // perameter
 {
-    printf("sum is : %d",x+y);
+    printf("sum is : %d\n", x + y);
+}
+void sub(int x, int y)
+{
+    printf("sub is : %d\n", x - y);
+}
+void mul(int x, int y)
+{
+    printf("mul is : %d\n", x * y);
+}
+void div(int x, int y)
+{
+    printf("div is : %d\n", x / y);
 }
