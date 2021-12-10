304e7ffda9d14f20f5430781df1b4ed5dadfd58c
chandrikabijore <chandrikabijore@gmail.com>
2021-12-10 18:28:13 +0530

    practice

--- 77-Function_practice.c
+++ 77-Function_practice.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 void student(int a, int b)
 {
+    printf("Enter any number : ");
+    scanf("%d", &a);
+    printf("Enter any number : ");
+    scanf("%d",&b);
+
     printf("hiii \n%d %d\nsum is : %d",a,b,a + b);
 }
 void main()
