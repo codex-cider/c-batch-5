db53f119417718641325f7f1364b8d60b687c90d
Rudransh waday <rudranshwaday2002@gmail.com>
2021-12-13 18:52:54 +0530

    area of circle

--- 37-area_of_circle_function_2.c
+++ 37-area_of_circle_function_2.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 void AreaOfCircle();
-void AreaOfCircle(int r)
+void AreaOfCircle()
 {
+    float r;
     printf("enter the radius :");
-    scanf("%d",&r);
+    scanf("%f",&r);
     float pi=3.14;
     float area_of_circle=pi*r*r;
     printf("enter the area of circle :%.2f",area_of_circle);
 }
 void main()
 {
-    float r;
-    AreaOfCircle(r);
+    AreaOfCircle();
 }
