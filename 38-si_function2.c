964b1e2856f89a61189fe5c5f380c9ba683241de
Rudransh waday <rudranshwaday2002@gmail.com>
2021-12-14 18:11:19 +0530

    calculator

---
+++ 38-si_function2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void si();
+void si()
+{
+    float p,r,t;
+   printf("enter the principal :");
+   scanf("%f",&p);
+   printf("enter the rate :");
+   scanf("%f",&r);
+   printf("enter the time :");
+   scanf("%f",&t);
+  float si=(p*r*t)/100;
+   printf("simple intrest :%.2f",si);
+}
+void main()
+{
+  si();
+}
---
+++ 39-condition_function.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void si();
+void AreaOfCircle();
+void si()
+{
+    float p,r,t;
+   printf("enter the principal :");
+   scanf("%f",&p);
+   printf("enter the rate :");
+   scanf("%f",&r);
+   printf("enter the time :");
+   scanf("%f",&t);
+  float si=(p*r*t)/100;
+   printf("simple intrest :%.2f",si);
+}
+void AreaOfCircle()
+{
+    float r;
+    printf("enter the radius :");
+    scanf("%f",&r);
+    float pi=3.14;
+    float area_of_circle=pi*r*r;
+    printf("enter the area of circle :%.2f",area_of_circle);
+}
+void main()
+{
+    int option;
+    printf("press 1 for simple intrest\n");
+    printf("press 2 for area of circle\n");
+    scanf("%d",&option);
+    if(option==1)
+    {
+       si();
+    }
+    else
+    {
+       AreaOfCircle();
+    }
+}
