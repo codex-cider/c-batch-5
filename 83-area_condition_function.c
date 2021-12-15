e6972c98f531e9c4dfd1dd93d0025a5f6c2f98f8
chandrikabijore <chandrikabijore@gmail.com>
2021-12-15 17:24:23 +0530

    volume_condition

--- 83-area_condition_function.c
+++ 83-area_condition_function.c
@@ -5,48 +5,52 @@ void area_of_rectangle();
 void area_of_circle();
 void area_of_triangle()
 {
-   float b,h;
+   float b, h;
    printf("enter the base :");
-   scanf("%f",&b);
+   scanf("%f", &b);
    printf("enter the hieght :");
-   scanf("%f",&h);
-   float area_of_triangle=(b*h)/2;
-   printf("Area of trianlge is :%.2f\n",area_of_triangle);
+   scanf("%f", &h);
+   float area_of_triangle=(b * h) / 2;
+   printf("Area of trianlge is : %.2f\n", area_of_triangle);
 }
+
 void area_of_square()
 {
     float a;
-    printf("enter the area:");
-    scanf("%f",&a);
-    float area_of_square=a*a;
-    printf("Area of square :%.2f\n",area_of_square);
+    printf("enter the area : ");
+    scanf("%f", &a);
+    float area_of_square = a * a;
+    printf("Area of square : %.2f\n", area_of_square);
 }
+
 void area_of_rectangle()
 {
-   int l,w;
+   int l, w;
    printf("enter the lenght :");
-   scanf("%f",&l);
-   printf("enter the width :");
-   scanf("%f",&w);
-   float area_of_rectangle=l*w;
-  printf("Area of rectangle :%.2f\n",area_of_rectangle);
+   scanf("%f", &l);
+   printf("enter the width : ");
+   scanf("%f", &w);
+   float area_of_rectangle = l * w;
+  printf("Area of rectangle : %.2f\n", area_of_rectangle);
 }
+
 void area_of_circle()
 {
     float r;
-    printf("enter the radius :");
-    scanf("%f",&r);
-    float pi=3.14;
-    float area_of_circle=pi*r*r;
-    printf("Area of circle is :%.2f\n",area_of_circle);
+    printf("enter the radius : ");
+    scanf("%f", &r);
+    float pi = 3.14;
+    float area_of_circle = pi * r * r;
+    printf("Area of circle is : %.2f\n", area_of_circle);
 }
+
 void main()
 {
     int option;
-    printf("Enter one for area of triangle\n");
-    printf("Enter two for square\n");
-    printf("Enter two for rectangle\n");
-    printf("Enter two for circle\n");
+    printf("Enter one for area of triangle \n");
+    printf("Enter two for square \n");
+    printf("Enter two for rectangle \n");
+    printf("Enter two for circle \n");
     scanf("%d",&option);
     if (option == 1)
     {
