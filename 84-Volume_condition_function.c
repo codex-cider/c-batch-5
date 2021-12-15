8a97ea5b1ac64ddbd14b1c380a2112dc6524359f
chandrikabijore <chandrikabijore@gmail.com>
2021-12-15 17:12:51 +0530

    volume_condition

---
+++ 84-Volume_condition_function.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void volume_of_cylinder();
+void volume_of_cube();
+void volume_of_cylinder()
+{
+   float r,h;
+   float pi=3.14;
+   printf("enter the radius :");
+   scanf("%f",&r); 
+   printf("enter the hieght :"); 
+   scanf("%f",&h);
+   float volume_of_cylinder=pi*r*r*h;
+   printf("volume of cylinder :%.2f\n",volume_of_cylinder);
+}
+void volume_of_cube()
+{
+   float a;
+   printf("enter the area :");
+   scanf("%f",&a); 
+   float volume_of_cube=a*a*a;
+   printf("volume of cube :%.2f\n",volume_of_cube);
+}
+void main()
+{
+   int option;
+    printf("Enter one for volume of cylinder\n");
+    printf("Enter two for volume of cube\n");
+    scanf("%d",&option);
+    if(option==1)
+    {
+        volume_of_cylinder();
+    }
+    if(option==2)
+    {
+        volume_of_cube();
+    }
+}
