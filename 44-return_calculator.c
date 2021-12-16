86a67114b2a7cce7bf68a3426c012bedc9a66e5f
Rudransh waday <rudranshwaday2002@gmail.com>
2021-12-16 19:06:06 +0530

    returne

--- 44-return_calculator.c
+++ 44-return_calculator.c
@@ -17,13 +17,36 @@ int div(int a,int b)
 }
 void main()
 {
-    int addition=sum(20,10);
-    printf("addition is :%d\n",addition);
-    int subtraction=sub(20,10);
-    printf("subtraction is :%d\n",subtraction);
-    int multiplication=mul(5,5);
-    printf("multiplication is :%d\n",multiplication);
-    int division =div(8,2);
-    printf("division is :%d\n",division);
+    int option;
+    printf("press one for sum");
+    printf("press two for sub");
+    printf("press three for mul");
+    printf("press four for div");
+    scanf("%d",&option);
+    if(option==1)
+    {
+        int addition=sum();
+        scanf("%d",&b);        
+        printf("enter the number a :");
+        scanf("%d",&a);
+        printf("enter the number b :");
+
+        printf("addition is :%d\n",addition);
+    }
+     if(option==2)
+     {
+       int subtraction=sub(20,10);
+       printf("subtraction is :%d\n",subtraction);
+     }
+     if (option==3)
+     {
+        int multiplication=mul(5,5);
+        printf("multiplication is :%d",multiplication);
+    }
+    if(option==4)
+    {
+        int division =div(8,2);
+        printf("division is :%d\n",division);
+    }
 
 }
