679fdc6fa99376ae0c6072596fe0d4d278ca6a13
sonal khetkar <khetkarsonal@gmail.com>
2021-12-17 11:04:41 +0530

    return calculator

--- 44-return_calculator.c
+++ 44-return_calculator.c
@@ -1,52 +1,71 @@
 #include<stdio.h>
 int sum(int a,int b)
-{
+{        
+    printf("enter the number a :");
+    scanf("%d",&a);
+    printf("enter the number b :");
+    scanf("%d",&b);  
     return a+b;
 }
 int sub(int a,int b)
 {
+    printf("enter the number a :");
+    scanf("%d",&a);
+    printf("enter the number b :");
+    scanf("%d",&b);  
     return a-b;
 }
 int mul(int a,int b)
 {
+    printf("enter the number a :");
+    scanf("%d",&a);
+    printf("enter the number b :");
+    scanf("%d",&b);  
     return a*b;
 }
 int div(int a,int b)
 {
+    printf("enter the number a :");
+    scanf("%d",&a);
+    printf("enter the number b :");
+    scanf("%d",&b);  
     return a/b;
 }
 void main()
 {
     int option;
-    printf("press one for sum");
-    printf("press two for sub");
-    printf("press three for mul");
-    printf("press four for div");
+    printf("press one for sum\n");
+    printf("press two for sub\n");
+    printf("press three for mul\n");
+    printf("press four for div\n");
     scanf("%d",&option);
     if(option==1)
     {
-        int addition=sum();
-        scanf("%d",&b);        
-        printf("enter the number a :");
-        scanf("%d",&a);
-        printf("enter the number b :");
-
+        int a,b;
+        int addition=sum(a,b); 
         printf("addition is :%d\n",addition);
     }
-     if(option==2)
+     else if(option==2)
      {
-       int subtraction=sub(20,10);
+        int a,b;
+       int subtraction=sub(a,b);
        printf("subtraction is :%d\n",subtraction);
      }
-     if (option==3)
+     else if (option==3)
      {
-        int multiplication=mul(5,5);
-        printf("multiplication is :%d",multiplication);
+        int a,b;
+        int multiplication=mul(a,b);
+        printf("multiplication is :%d\n",multiplication);
     }
-    if(option==4)
+    else if(option==4)
     {
-        int division =div(8,2);
+        int a,b;
+        int division =div(a,b);
         printf("division is :%d\n",division);
+    }
+    else
+    {
+        printf("wrong option\n");
     }
 
 }
