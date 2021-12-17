7ecb8810bc283c5289900e5d83c2492c28f3df60
chandrikabijore <chandrikabijore@gmail.com>
2021-12-17 17:00:31 +0530

    function_calculator

--- 87-return_calculator.c
+++ 87-return_calculator.c
@@ -1,32 +1,67 @@
 #include<stdio.h>
 int sum(int a, int b)
 {
+    printf("Enter here first number : ");
+    scanf("%d", &a);
+    printf("Enter here second number : ");
+    scanf("%d", &b);
     return a + b;
 }
 int sub(int a, int b)
 {
+    printf("Enter here first number : ");
+    scanf("%d", &a);
+    printf("Enter here second number : ");
+    scanf("%d", &b);
     return a - b;
 }
 int mul(int a, int b)
 {
+    printf("Enter here first number : ");
+    scanf("%d", &a);
+    printf("Enter here second number : ");
+    scanf("%d", &b);
     return a * b;
 }
-float div(int a, int b)
+float div(float a, float b)
 {
+    printf("Enter here first number : ");
+    scanf("%f", &a);
+    printf("Enter here second number : ");
+    scanf("%f", &b);
     return a / b;
 }
-
 void main()
-{
-    int Addition = sum(15, 10);
-    printf("Addition is : %d\n", Addition);
-
-    int subtraction = sub(15, 10);
-    printf(" subtraction is : %d\n", subtraction);
-
-    int multiplication = mul(15, 10);
-    printf(" multiplication  is : %d\n", multiplication );
-
-    float divition = div(15, 10);
-    printf(" divition is : %f\n", divition);
+{   
+    int a,b;
+    int option;
+    printf("Enter one for sum : \n");
+    printf("Enter two for sub : \n");
+    printf("Enter two for mul : \n");
+    printf("Enter two for sub : \n");
+    scanf("%d", &option);
+    if(option == 1)
+    {
+        int addition = sum(a,b);
+        printf("addition is : %d\n",addition);
+    }
+    else if (option == 2)
+    {
+        int substraction = sub(a,b);
+        printf("substraction is : %d\n",substraction);
+    }
+    else if (option == 3)
+    {
+        int multiplication = mul(a,b);
+        printf("multiplication is : %d\n",multiplication);
+    }
+    else if (option == 4)
+    {
+        float divison = div(a,b);
+        printf("divison is : %f\n",divison);
+    }
+    else
+    {
+        printf("\nThis is out of option");
+    }
 }
