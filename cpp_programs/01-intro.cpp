a48886ff7bd1c1297ce7f04732a1b0eff3e50ec4
Rudransh waday <rudranshwaday2002@gmail.com>
2021-12-21 18:21:53 +0530

    sum

---
+++ cpp_programs/01-intro.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+int main()
+{
+    std::cout<<"hi my name is sonal";
+}
+#include<iostream>
+int main()
+{
+    std::cout<<"my name is sonal";
+}
+#include<iostream>
+int main()
+{
+    std::cout<<"hi my name is sonal";
+}
+#include<iostream>
+int main()
+{
+    std::cout<<"my name is sonal";
+}
+#include<iostream>
+int main()
+{
+    std::cout<<"hi my name is sonal";
+}
---
+++ cpp_programs/02-take_inpute_from_user.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+    int a,b;
+    std::cout<<"enter the value of a :";
+    std::cin>>a;
+    std::cout<<"enter the value of b :";
+    std::cin>>b;
+    std::cout<<"sum of a & b :"<<a+b;
+}
