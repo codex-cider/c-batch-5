11064bd70a69fa2bee296fec2383400d43b6d9da
Prachi <prachimeena968@gmail.com>
2021-12-21 18:21:34 +0530

    user

---
+++ c++/class.cpp
@@ -0,0 +1,14 @@
+// cout is used to print string
+// in c++ we only use int becuse compiler haqs set the restrctions
+// std= namespace
+//::=to access
+
+#include <iostream>
+int main()
+{
+    std::cout << "hello my self prachi\n";
+    std::cout << "i am 19 years old\n";
+    std::cout << "i leave in indore\n";
+    std::cout << "i love dancing\n";
+    std::cout << "my proffesion is engineer";
+}
---
+++ c++/take_input_user.cpp
@@ -0,0 +1,12 @@
+// cin = user sai value laiga
+
+#include <iostream>
+int main()
+{
+    int a, b;
+    std::cout << "enter first number-\n";
+    std::cin >> a;
+    std::cout << "enter second number-\n";
+    std::cin >> b;
+    std::cout << "sum of both number is:" << a + b;
+}
