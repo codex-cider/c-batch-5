be0a869291d040ce42abe2338dfdc3065675e022
Rudransh waday <rudranshwaday2002@gmail.com>
2021-12-22 18:40:55 +0530

    calculator

--- cpp_programs/04-calculator.cpp
+++ cpp_programs/04-calculator.cpp
@@ -1,26 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class Calculator
 {
     int sum()
     {
-        cout<<"sum is called";
+        cout << "sum is called";
     }
     int sub()
     {
-        cout<<"sub is called";
+        cout << "sub is called";
     }
     int mul()
     {
-        cout<<"mul is called";
+        cout << "mul is called";
     }
     int div()
     {
-        cout<<"div is called";
+        cout << "div is called";
     }
-
 };
 int main()
 {
-    int calculator;
+    calculator x;
 }
