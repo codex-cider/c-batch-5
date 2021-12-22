538c240ff8c6314b9a939861033637af863224ff
Rudransh waday <rudranshwaday2002@gmail.com>
2021-12-22 18:25:36 +0530

    class

--- cpp_programs/02-take_inpute_from_user.cpp
+++ cpp_programs/02-take_inpute_from_user.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
+using namespace std;
 int main()
 {
     int a,b;
-    std::cout<<"enter the value of a :";
-    std::cin>>a;
-    std::cout<<"enter the value of b :";
-    std::cin>>b;
-    std::cout<<"sum of a & b :"<<a+b;
+    cout<<"enter the value of a :";
+    cin>>a;
+    cout<<"enter the value of b :";
+    cin>>b;
+    cout<<"sum of a & b :"<<a+b;
+    printf("\nsum of a & b :%d",a+b);
+    printf("\nsum of %d & %d is :%d",a,b,a+b);
+    cout<<"\nsum of "<<a<<" & "<<b<<" is :"<<a+b;
 }
---
+++ cpp_programs/03-class.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class student
+{
+    public:
+    int name();
+    int name()
+    {
+        cout<<"hi";
+    }
+};
+int main()
+{
+    student b; 
+    b.student;
+    b.name();
+}
+
