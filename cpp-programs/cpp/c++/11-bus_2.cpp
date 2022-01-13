d33f712eb9fff17ac4387a57b05896bafe92a922
chandrikabijore <chandrikabijore@gmail.com>
2022-01-13 18:08:15 +0530

    Getdetails

--- cpp-programs/cpp/c++/11-bus_2.cpp
+++ cpp-programs/cpp/c++/11-bus_2.cpp
@@ -9,6 +9,22 @@ class Bus
     string destination;
     string type;
 };
+class BusOpration 
+{
+    public : 
+    Bus bus;
+    void getDetails()
+    {
+        cout<<"How many seats are in bus : ";
+        cin>>bus.seats;
+        cout<<"what is the starting point of this bus : ";
+        cin>>bus.startingPoint;
+        cout<<"what is the destination of this bus : ";
+        cin>>bus.destination;
+        cout<<"What is the type of this bus : ";
+        cin >>bus.type;
+    }
+};
 int main()
 {
    
