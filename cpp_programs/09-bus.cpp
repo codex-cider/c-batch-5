c9c824c706a2a2ed1930574d369b0cce7f22e0ff
sonal khetkar <khetkarsonal@gmail.com>
2022-01-13 17:47:04 +0530

    bus

--- cpp_programs/09-bus.cpp
+++ cpp_programs/09-bus.cpp
@@ -1,20 +1,13 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class BusOpration
+class Bus
 {
     public:
     int seats;
     string startingPoint;
     string endingPoint;
     string type;
-    void getDetail();
-    void getDetail()
-   {
-       this.seats;
-       this.startingPoint;
-       this.endingPoint;
-   }
 };
 int main()
 {
