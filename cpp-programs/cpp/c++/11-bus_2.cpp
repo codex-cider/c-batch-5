febb9597e8ff8cf2d70adf1705818e15b7cefa86
chandrikabijore <chandrikabijore@gmail.com>
2022-01-13 18:11:38 +0530

    printDetails

--- cpp-programs/cpp/c++/11-bus_2.cpp
+++ cpp-programs/cpp/c++/11-bus_2.cpp
@@ -24,6 +24,17 @@ class BusOpration
         cout<<"What is the type of this bus : ";
         cin >>bus.type;
     }
+    void printDetails()
+    {
+        cout<<"How many seats are in bus : ";
+        cin>>bus.seats;
+        cout<<"what is the starting point of this bus : ";
+        cin>>bus.startingPoint;
+        cout<<"what is the destination of this bus : ";
+        cin>>bus.destination;
+        cout<<"What is the type of this bus : ";
+        cin >>bus.type;
+    }
 };
 int main()
 {
