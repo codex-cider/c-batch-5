6df2b88338f9abdc4083d0c91de303c9bd6ddd84
sonal khetkar <khetkarsonal@gmail.com>
2022-01-13 17:38:53 +0530

    bus

--- cpp_programs/09-bus.cpp
+++ cpp_programs/09-bus.cpp
@@ -1,31 +1,22 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class Bus
+class BusOpration
 {
     public:
     int seats;
     string startingPoint;
     string endingPoint;
     string type;
+    void getDetail();
+    void getDetail()
+   {
+       this.seats;
+       this.startingPoint;
+       this.endingPoint;
+   }
 };
 int main()
 {
-    Bus x;
-    x.seats;
-    x.startingPoint;
-    x.endingPoint;
-    x.type;
-    cout<<"How many seats in the Bus :";
-    cin>>x.seats;
-    cout<<"what is the starting point of bus :";
-    cin>>x.startingPoint;
-    cout<<"what is the ending point of bus :";
-    cin>>x.endingPoint;
-    cout<<"Type of bus :";
-    cin>>x.type;
-    cout<<"\nHow many seats in the Bus :"<<x.seats;
-    cout<<"\nwhat is the starting point of bus :"<<x.startingPoint;
-    cout<<"\nwhat is the ending point of bus :"<<x.endingPoint;
-    cout<<"\nType of bus :"<<x.type;
+   
 }
