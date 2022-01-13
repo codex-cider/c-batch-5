4eeccd747cc27a1191c72b2fd07e9933d9fceed1
chandrikabijore <chandrikabijore@gmail.com>
2022-01-13 17:42:28 +0530

    Bus class

---
+++ cpp-programs/cpp/c++/10-bus.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+class bus
+{
+    public :
+    int seats;
+    string startingPoint;
+    string destination;
+    string type;
+};
+int main()
+{
+    bus x;
+    x.seats;
+    x.startingPoint;
+    x.destination;
+    cout<<"How many seats are in bus : ";
+    cin>>seats;
+    cout<<"what is the starting point of this bus : ";
+    cin>>startingPoint;
+    cout<<"what is the destination of this bus : ";
+    cin>>destination;
+    cout<<"what is the type of this bus : ";
+    cin>>destination;
+}
---
+++ cpp-programs/cpp/c++/11-bus_2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class BusOpration
+{
+    public:
+    int seats;
+    string startingPoint;
+    string endingPoint;
+    string type;
+    void getDetail();
+    void getDetail()
+   {
+       this.seats;
+       this.startingPoint;
+       this.endingPoint;
+   }
+};
+int main()
+{
+   
+}
