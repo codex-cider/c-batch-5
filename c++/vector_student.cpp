7a030cc6a20fbb96c4dbc530a5319f010e03bf98
Prachi <prachimeena968@gmail.com>
2022-01-17 18:49:56 +0530

    vector_student

--- c++/vector_student.cpp
+++ c++/vector_student.cpp
@@ -2,12 +2,25 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <string>
+#include <vector>
 using namespace std;
-class Student;
+class Student
 {
 public:
-    string Name, Roll Number, Email, Mobile;
+    string Name, RollNumber, Email, Mobile;
 };
 int main()
 {
+    vector<Student> Student;
+
+    Student S1;
+    S1.Name = "Prachi";
+    Student.push_back(S1);
+    S1.RollNumber = "22";
+    Student.push_back(S1);
+    S1.Email = "prachigothwal52@gmail.com";
+    Student.push_back(S1);
+    S1.Mobile = "9617001332";
+    Student.push_back(S1);
 }
