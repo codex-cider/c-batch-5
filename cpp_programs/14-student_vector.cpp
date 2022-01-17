f1d25d865b391842b87121b4f7a09e92b079fa42
sonal khetkar <khetkarsonal@gmail.com>
2022-01-17 18:50:03 +0530

    student vactor

---
+++ cpp_programs/14-student_vector.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class Student
+{
+   public:
+   string Name,Email;
+   int Rollnumber,Mobile;
+
+};
+int main()
+{
+   vector<Student>students;
+   Student s1;
+   s1.Name="ravi";
+   students.push_back(s1);
+   s1.Email="kjshh@gmail.com";
+   students.push_back(s1);
+   s1.Rollnumber=123;
+   students.push_back(s1);
+   s1.Mobile=123456;
+   students.push_back(s1);
+   cout<<s1.Name<<endl;
+   cout<<s1.Email<<endl;
+   cout<<s1.Rollnumber<<endl;
+   cout<<s1.Mobile<<endl;
+}
