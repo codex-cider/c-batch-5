4c017466a55665807e6e8c269b33c351e82850e4
Prachi <prachimeena968@gmail.com>
2022-01-17 18:01:52 +0530

    vector_char.cpp

---
+++ c++/vector_char.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    vector<string> character;
+    character.push_back("a");
+    character.push_back("e");
+
+    character.push_back("i ");
+
+    for (int i = 0; i < character.size(); i++)
+    {
+        cout << character.at(i) << endl;
+    }
+
+    //array
+    string n[3];
+    n[0] = "a";
+    n[1] = "e";
+    n[2] = "i";
+}
