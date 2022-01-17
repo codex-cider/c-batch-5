dc0001cee4ff5cf56d64b9090702579c7b5d33a5
Prachi <prachimeena968@gmail.com>
2022-01-17 17:59:42 +0530

    vector_prime

---
+++ c++/vector_prime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    vector<string> number;
+    number.push_back("2");
+    number.push_back("3");
+
+    number.push_back("5 ");
+
+    for (int i = 0; i < number.size(); i++)
+    {
+        cout << number.at(i) << endl;
+    }
+
+    //array
+    string n[3];
+    n[0] = "2";
+    n[1] = "3";
+    n[2] = "5";
+}
