47aa74cdb828c77c13008fcc654cf9ea24915a4d
Krishnakant Madhpuriya <kkmadhpuriya@gmail.com>
2022-01-17 18:01:16 +0530

    Vector

---
+++ cpp-programs/vector.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    vector<string> names;
+    names.push_back("Devendra Yadav");
+    names.push_back("Ravi Verma");
+    names.push_back("Rahul Verma");
+
+    for (int i = 0; i < names.size(); i++)
+    {
+        cout << names.at(i) << endl; // a[i] --> names.at(i)
+    }
+
+    // Array
+    string n[3];
+    n[0] = "Devendra Yadav";
+    n[1] = "Ravi Verma";
+    n[2] = "Rahul Verma";
+}
