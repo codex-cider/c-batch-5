45cc3a1a2947284338c51354f043d810ec72a568
sonal khetkar <khetkarsonal@gmail.com>
2022-01-19 19:24:05 +0530

    project

--- cpp_programs/15-student_vector2.cpp
+++ cpp_programs/15-student_vector2.cpp
@@ -7,27 +7,30 @@ class Student
    public:
    string Name,Email,Mobile;
    int Rollnumber;
-
 };
 int main()
 {
-   
-    vector<Student>students;
+     vector<Student>students;
    for(int i=0;i<5;i++)
    {
-        Student s;
+       Student s;
        cout<<"Enter students name :";
-       cin>>s.Name;
+       getline(cin,s.Name);
+       //cin>>s.Name;
        cout<<"Enter students email:";
-       cin>>s.Email;
+       getline(cin,s.Email);//puri line prine
+       //cin>>s.Email;
+       cout<<"Enter the mobile number :";
+       getline(cin,s.Mobile);
+       //cin>>s.Mobile;
        cout<<"Enter students rollnumber :";
        cin>>s.Rollnumber;
-       cout<<"Enter the mobile number :";
-       cin>>s.Mobile;
+       cin.ignore();
        students.push_back(s);
        cout<<s.Name<<endl;
        cout<<s.Email<<endl;
        cout<<s.Rollnumber<<endl;
        cout<<s.Mobile<<endl;
+       cout<<"\n";
    }
 }
---
+++ cpp_programs/student_project_1/student.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Student
+{
+   void add_students()
+   {
+       cout<<"Add students name:";
+   }
+   void print_all_students()
+   {
+       cout<<"Print all students name :";
+   }
+   void search_students()
+   {
+       cout<<"search student name :";
+   }
+}
