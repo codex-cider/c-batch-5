f6ca33812b1e7007be9a96d53fac7e3e30fa1c27
chandrikabijore <chandrikabijore@gmail.com>
2022-02-20 18:08:17 +0530

    inheritance

---
+++ cpp-programs/cpp/c++/student_project_1/project/OOP/05-inheritance.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<string>
+#include<iostream>
+using namespace std;
+class college
+{
+    string ownerName;
+
+protected : 
+    string collegeName;
+
+public :
+    college(string collegeName)
+    {
+        this->collegeName = collegeName;
+    }
+};
+
+class student : public college
+{
+protected :
+    string name, rollNumber;
+
+public :
+    student(string n, string rollN ,string collegeName) : college(collegeName)
+    {
+        name = n;
+        rollNumber = rollN;
+    }
+
+    void printStudentinfo()
+    {
+        cout << "\n--------------------------------------------------------\n";
+        cout << "college name " << this->collegeName <<endl;
+        cout << "student name " << this->name<<endl;
+        cout << "Roll name " << this->rollNumber <<endl;
+    }
+};
+
+void main()
+{
+    string name, rollNumber, collegeName;
+
+    cout << "Enter your name : ";
+    getline(cin, name);
+
+    cout << "Enter your Roll Number  : ";
+    getline(cin, rollNumber);
+
+    cout << "Enter your college name : ";
+    getline(cin, collegeName);
+    
+}
